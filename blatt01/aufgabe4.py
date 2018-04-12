9673fac2f0001e15ebd7a69353ba893770df51b6
mdbug <michael_vb@web.de>
2018-04-12 21:31:30 +0200

    Blatt01 Aufg 4 WIP

---
+++ blatt01/aufgabe4.py
@@ -0,0 +1,34 @@
+import numpy as np
+from math import log
+
+
+def integral_trapez(n, a, b, f):
+    h = (b - a)/n
+    v = np.arange(1, n)
+    v = a + h*v
+    v = f(v)
+    return h/2 * (f(a) + 2*np.sum(v) + f(b))
+
+
+def f(x):
+    return 1/(x**2)
+
+
+def g(x):
+    return log(x)
+
+
+n = 1000
+a = 0.1
+b = 10
+int_f_trapez = integral_trapez(n, a, b, f)
+int_f_exakt = 9.9
+print("n = %d; int_a^b f:" % n)
+print("trapez = %f, exakt = %f" % (int_f_trapez, int_f_exakt))
+
+a = 1
+b = 2
+int_g_trapez = integral_trapez(n, a, b, g)
+int_g_exakt = log(4) - 1
+print("n = %d; int_a^b g:" % n)
+print("trapez = %f, exakt = %f" % (int_g_trapez, int_g_exakt))
