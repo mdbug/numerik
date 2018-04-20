7a523bca7f2df01c38ada27999527e9d021038b2
mdbug <michael@bothmer.name>
2018-04-20 15:36:18 +0200

    blatt2 aufg1 wip

--- blatt02/aufgabe1.py
+++ blatt02/aufgabe1.py
@@ -1,31 +1,47 @@
 from numpy import array, zeros, copy
 
 
-def swap_rows(a, i, j):
-    tmp = copy(a[i, :])
-    a[i, :] = a[j, :]
-    a[j, :] = tmp
+def swap_rows(matrix, row_1, row_2):
+    tmp = copy(matrix[row_1, :])
+    matrix[row_1, :] = matrix[row_2, :]
+    matrix[row_2, :] = tmp
 
 
 def zerlegung(a):
     lu = array(a, copy=True)
     n = a.shape[0]
-    p = zeros(n)
-    for i in range(n-1):
+    p = zeros(n-1)
+    for i in range(n):
         if lu[i, i] == 0:
             k = i
             while lu[k, i] == 0:
                 k += 1
 
+            p[i] = k+1
             # Tausche Zeile i mit k
-            swap_rows(a, i, k)
-
-            p[i] = lu[k, i]
+            swap_rows(lu, i, k)
 
         for j in range(i + 1, n):
             lu[j, i] /= lu[i, i]
+            for k in range(i + 1, n):
+                lu[j, k] -= (lu[j, i]*lu[i, k])
 
     return lu, p
+
+
+def permutation(p, x):
+    for i in range(x.size):
+        tmp = x[i]
+        x[i] = x[p[i] - 1]
+        x[p[i] - 1] = tmp
+
+
+def vorwaerts(lu, x):
+    # TODO
+
+
+def rueckwaerts(lu, x):
+    # TODO
 
 
 a = array([[0, 0, 0, 1], [2, 1, 2, 0], [4, 4, 0, 0], [2, 3, 1, 0]])
@@ -33,7 +49,3 @@ lu, p = zerlegung(a)
 
 print(lu)
 print(p)
-
-
-
-
