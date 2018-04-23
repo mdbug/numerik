e410595ac658bb7cc16d4a8d788b8c2686c98f2a
Michael von Bothmer <michael.bothmer@alumni.fh-aachen.de>
2018-04-23 20:41:32 +0200

    Blatt 2 Aufgabe 3

--- blatt02/aufgabe3.py
+++ blatt02/aufgabe3.py
@@ -1,15 +1,41 @@
-from numpy import array, zeros, sqrt
+from numpy import array, zeros, sqrt, copy
 
 
-def cholesky(a: array) -> array:
-    d1 = zeros(a.size)
-    d2 = zeros(a.size - 1)
+def cholesky(d1: array, d2: array) -> (array, array):
+    l_d1 = copy(d1)
+    l_d2 = copy(d2)
 
-    for i in range(a.size):
-        d1 = a[i, i]
+    for i in range(l_d1.size):
+        l_d1[i] = sqrt(l_d1[i])
+        if i < l_d1.size - 1:
+            l_d2[i] /= l_d1[i]
+            l_d1[i + 1] -= l_d2[i]**2
 
-    for i in range(a.size-1):
-        d2 = a[i+1, i]
+    return l_d1, l_d2
 
-    for i in range(d1):
-        d1[i] = sqrt(i)
+
+def diagonalmatrix(d1: array, d2: array) -> array:
+    mat = zeros([d1.shape[0], d1.shape[0]])
+    for i in range(d1.shape[0]):
+        mat[i, i] = d1[i]
+
+    for i in range(d2.shape[0]):
+        mat[i +1, i] = d2[i]
+
+    return mat
+
+
+for n in [100, 1000, 10000]:
+    print("n = %d" % n)
+
+    # Initialisiere Diagonalen
+    d1 = array([2.0] * n)
+    d2 = array([-1.0] * (n - 1))
+
+    l_d1, l_d2 = cholesky(d1, d2)
+    print("Hauptdiagonale")
+    print(l_d1)
+    print("Nebendiagonale 1")
+    print(l_d2)
+    print("-" * 20)
+
