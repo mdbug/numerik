482ee869c267fd801084f16995680c821bd4f280
Michael von Bothmer <michael.bothmer@alumni.fh-aachen.de>
2018-05-08 20:27:32 +0200

    Blatt 4 Aufgabe 1 fertig

--- blatt04/aufgabe1.py
+++ blatt04/aufgabe1.py
@@ -50,6 +50,17 @@ def vorwaerts(lu: array, x: array) -> array:
     return y
 
 
+def vorwaerts2(lu: array, x: array) -> array:
+    y = copy(x)
+    for i in range(x.size):
+        for j in range(i):
+            y[i] -= lu[i, j]*y[j]
+
+        y[i] /= lu[i,i]
+
+    return y
+
+
 def rueckwaerts(lu: array, x: array) -> array:
     y = copy(x)
     for i in reversed(range(x.size)):
@@ -62,11 +73,15 @@ def rueckwaerts(lu: array, x: array) -> array:
 
 
 def berechne_v(lu: array, c: array) -> array:
-    return vorwaerts(lu.T, c)
+    return vorwaerts2(lu.T, c)
 
 
 def berechne_w(lu: array, v: array) -> array:
-    return rueckwaerts(lu.T, v)
+    l = copy(lu)
+    for i in range(l.shape[0]):
+        l[i, i] = 1.0
+
+    return rueckwaerts(l.T, v)
 
 
 def berechne_z(p: array, w: array) -> array:
@@ -81,7 +96,6 @@ def berechne_z(p: array, w: array) -> array:
 
 def loese(a: array, c: array) -> array:
     lu, p = zerlegung_pivot(a)
-    print(lu)
     v = berechne_v(lu, c)
     w = berechne_w(lu, v)
     return berechne_z(p, w)
@@ -95,4 +109,3 @@ a = array([[0, 0, 0, 1],
 c = array([152, 154, 56, 17], dtype=float)
 
 print(loese(a, c))
-print(solve(a.T, c))
